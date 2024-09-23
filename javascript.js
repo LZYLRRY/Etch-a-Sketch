92f07171b6b5a2258904141c2e423b2af4b7d5c6
LZYLRRY <162039530+LZYLRRY@users.noreply.github.com>
2024-09-23 18:45:36 +0200

    Add Style

---
+++ javascript.js
@@ -0,0 +1,12 @@
+const container = document.querySelector('.container');
+
+function addGrid() {
+    for (i = 0; i < 256; i++) {
+        const div = document.createElement('div');
+        div.classList = 'grid'
+    
+        container.appendChild(div)
+    }
+}
+
+addGrid()
