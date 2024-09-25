932fa5c6d0a0c2dd948b2db3ec2367e397d61b29
LZYLRRY <162039530+LZYLRRY@users.noreply.github.com>
2024-09-25 04:10:24 +0200

    Create slider and grid

--- javascript.js
+++ javascript.js
@@ -1,12 +1,24 @@
 const container = document.querySelector('.container');
+const slider = document.querySelector('#sizeSlider');
+const sizeValue = document.querySelector('#sizeValue');
 
-function addGrid() {
-    for (i = 0; i < 256; i++) {
+slider.addEventListener('click', () => {
+    sizeValue.textContent = `${slider.value} X ${slider.value}`
+    addGrid(slider.value)
+});
+
+function addGrid(valueSlider) {
+    container.innerHTML = ''; // Remove existing div
+
+    for (i = 0; i < (valueSlider * valueSlider); i++) {
         const div = document.createElement('div');
         div.classList = 'grid'
-   
+
+        div.style.width = `${500 / valueSlider}px`;
+        div.style.height = `${500 / valueSlider}px`;
+
         container.appendChild(div)
     }
 }
 
-addGrid()
+addGrid(slider.value);
